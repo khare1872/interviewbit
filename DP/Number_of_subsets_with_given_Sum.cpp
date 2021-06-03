2feaec805877d0b66983fa94338dc1478a1b3959
SHUBHAM KHARE <69118006+khare1872@users.noreply.github.com>
2021-06-03 12:49:10 +0530

    Number_of_ways_of_coin_change.

--- DP/Number_of_subsets_with_given_Sum.cpp
+++ DP/Number_of_subsets_with_given_Sum.cpp
@@ -8,7 +8,6 @@ public:
       //initialization
        for(int i =0;i<n+1;i++) dp[0][i]=0;
        for(int i=0;i<m+1;i++) dp[i][0]=1;
-      // main part
        for(int i=1;i<m+1;i++){
            for(int j=1;j<n+1;j++){
               if(j>=S[i-1]) dp[i][j]=dp[i-1][j]+dp[i][j-S[i-1]];
