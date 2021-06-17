c67e2c917cd53eab06f1d3e4a0878837b9e4b39d
SHUBHAM KHARE <69118006+khare1872@users.noreply.github.com>
2021-06-17 12:29:49 +0530

    Create Max_Depth_of_Binary_trees.cpp

---
+++ Trees/Max_Depth_of_Binary_trees.cpp
@@ -0,0 +1,12 @@
+ void solve(TreeNode* node,int d,int& count){
+        if(node==NULL) return ;
+        count=max(count,d);
+        solve(node->left,d+1,count);
+        solve(node->right,d+1,count);
+    }
+int Solution::maxDepth(TreeNode* A) {
+    int count =0;
+    solve(A,0,count);
+    return count+1;
+    
+}
