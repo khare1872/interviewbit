f2e82d7d7165abaf4740aa5d51f6cfb6a265656d
SHUBHAM KHARE <69118006+khare1872@users.noreply.github.com>
2021-06-20 01:11:30 +0530

    Update Sum_root_to_leaf.cpp

--- Trees/Sum_root_to_leaf.cpp
+++ Trees/Sum_root_to_leaf.cpp
@@ -1,34 +1,29 @@
 /**
- * Definition for a binary tree node.
+ * Definition for binary tree
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
-class Solution {
-public:
-    void solve(TreeNode* root,int &sum,int &ans){
+  void solve(TreeNode* root,int sum,int &ans){
         if(!root) {
             
             return;
         }
-        sum=sum*10+root->val;
-        if(!root->left&&!root->right) ans+=sum;
+        sum=(sum*10+root->val)%1003;
+        if(!root->left&&!root->right) ans=(ans+sum)%1003;
+        else {
         solve(root->left,sum,ans);
         solve(root->right,sum,ans);
         sum/=10;
+        }
         
     }
-    int sumNumbers(TreeNode* root) {
-        int sum = 0;
-        int ans = 0;
-        solve(root,sum,ans);
-        cout<<endl;
-        return ans;
-        
-    }
-};
+int Solution::sumNumbers(TreeNode* root) {
+    int sum =0;
+    int ans =0;
+    solve(root,sum,ans);
+return ans;
+}
