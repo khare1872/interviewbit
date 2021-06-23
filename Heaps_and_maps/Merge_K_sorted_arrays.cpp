a83e7c9f9292d008176f1c7ac41617378f53f602
SHUBHAM KHARE <69118006+khare1872@users.noreply.github.com>
2021-06-23 22:56:26 +0530

    Update Merge_K_sorted_arrays.cpp

--- Heaps_and_maps/Merge_K_sorted_arrays.cpp
+++ Heaps_and_maps/Merge_K_sorted_arrays.cpp
@@ -16,3 +16,32 @@ vector<int> Solution::solve(vector<vector<int> > &A) {
     }
     return v3;
 }
+//GFG SOLUTION
+class Solution
+{
+    public:
+    //Function to merge k sorted arrays.
+    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
+    {   
+        int n = arr.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        for(int i =0;i<K;i++){
+            pq.push({arr[i][0],i*n});
+        }
+        
+        vector<int> ans;
+        while(!pq.empty()){
+            pair<int,int> current = pq.top();
+            pq.pop();
+            ans.push_back(current.first);
+            int i = current.second;
+           // cout<<i<<endl;
+            int index = i%n;
+            if(index<K-1){
+                int row_num = i/n;
+                pq.push({arr[row_num][index+1],(row_num*n+index+1)});
+            }
+        }
+        return ans;
+    }
+};
